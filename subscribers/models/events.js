e4a10a28b7b47f8141da64ec9ec34eae76a29fbc
Stephen Young <stephen101@gmail.com>
2019-11-25 16:08:01 +0200

    Log events to mongodb

---
+++ subscribers/models/events.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const eventSchema = new mongoose.Schema( { 
+    name : {
+        type : String,
+        required: true
+    },
+    eventId : {
+        type : String,
+        required : true
+    },
+    data : mongoose.Mixed
+});
+
+module.exports = mongoose.model("Events", eventSchema);
--- subscribers/paymentScheduleSubscribers.js
+++ subscribers/paymentScheduleSubscribers.js
@@ -1,43 +1,65 @@
-var kafka = require("kafka-node"),
+require('dotenv').config()
+const mongoose = require("mongoose");
+const Event = require("./models/events.js")
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection
+db.on("error", (error) => console.error(error) );
+db.once("open", () => console.log("Connected to database") );
+
+const kafka = require("kafka-node"),
   Consumer = kafka.Consumer,
   client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' }),
   consumer = new Consumer(client, [{ topic: "contract-events", partition: 0 }], {
     autoCommit: false
   });
 
-var messageHandlers = {
-  "PaymentScheduleCreated" : (details) => {
-      var event = {};
+const messageHandlers = {
+  "PaymentScheduleCreated" : async (details) => {
+      var event = new Event();
       event.name = details.name;
-      event.id = details.nonIndexedParameters[0].value;
-      event.paymentLeeway = details.nonIndexedParameters[1].value;
-      event.owner = details.nonIndexedParameters[2].value;
-      event.destination = details.nonIndexedParameters[3].value;
-      event.subscriptionAmount = details.nonIndexedParameters[4].value;
-      console.log(event);
+      event.eventId = details.id;
+      event.data = {
+        id : details.nonIndexedParameters[0].value,
+        paymentLeeway : details.nonIndexedParameters[1].value,
+        owner : details.nonIndexedParameters[2].value,
+        destination : details.nonIndexedParameters[3].value,
+        subscriptionAmount : details.nonIndexedParameters[4].value
+      }
+      return event;
     },
-  "NextPaymentDateSet" : (details) => {
-      var event = {};
+  "NextPaymentDateSet" : async (details) => {
+      var event = new Event();
       event.name = details.name;
-      event.id = details.nonIndexedParameters[0].value;
-      event.nextPaymentDate = new Date(details.nonIndexedParameters[1].value * 1000);
-      console.log(event);
+      event.eventId = details.id;
+      event.data = {
+        id : details.nonIndexedParameters[0].value,
+        nextPaymentDate : new Date(details.nonIndexedParameters[1].value * 1000)
+      }
+      return event;
     },
-  "Deposit" : (details) => {
-      var event = {};
+  "Deposit" : async (details) => {
+      var event = new Event();
       event.name = details.name;
-      event.owner = details.nonIndexedParameters[0].value;
-      event.amount = details.nonIndexedParameters[1].value;
-      console.log(event);
+      event.eventId = details.id;
+      event.data = {
+        owner : details.nonIndexedParameters[0].value,
+        amount : details.nonIndexedParameters[1].value,
+      }
+      return event;
     }
 }
 
-consumer.on("message", function(message) {
+consumer.on("message", async function(message) {
   let msgObj = JSON.parse(message.value);
-  //messageHandlers[msgObj.details.name](msgObj);
-  // msgObj.details.nonIndexedParameters.forEach(param => {
-  //   console.log(param);
-  // });
-  messageHandlers[msgObj.details.name](msgObj.details);
-  console.log();
+  try {
+    const eventAlreadyLogged = await Event.exists({ eventId: msgObj.details.id });
+    if (!eventAlreadyLogged) {
+      const event = await messageHandlers[msgObj.details.name](msgObj.details);
+      const newEvent = await event.save();
+      console.log(newEvent);
+    }
+  } catch (error) {
+    console.error(error);
+  }
 });
