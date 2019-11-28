0632b2332a05166a31f544707d88178a22c17ba0
Stephen Young <stephen101@gmail.com>
2019-11-28 15:24:47 +0200

    Script to advance block time every n seconds

---
+++ subscribers/advanceTime.js
@@ -0,0 +1,11 @@
+const {setIntervalAsync} = require('set-interval-async/dynamic');
+const testHelper = require("../../test/helpers/truffleTestHelper");
+
+setIntervalAsync(
+    async () => { 
+        const daysToAdvance = 1;
+        console.log(`Advancing time by ${daysToAdvance} days`);
+        const newBlock = await testHelper.advanceTimeAndBlock(testHelper.daysToSeconds(1));
+    },
+    3000
+);
